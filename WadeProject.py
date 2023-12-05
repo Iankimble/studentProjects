051ade116dc6a070a0de8272a3cb3fde9da52054
Ian <38480036+Iankimble@users.noreply.github.com>
2023-12-05 18:10:42 +0000

    first commit

---
+++ WadeProject.py
@@ -0,0 +1,82 @@
+
+import random
+
+a = "yes"
+
+while a == "yes":
+    number = random.randint(1, 6)
+
+    if number == 1:
+        print("[-----]") 
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print(       )
+    
+    if number == 2:
+        print("[-----]") 
+        print("[0    ]")
+        print("[     ]")
+        print("[    0]")
+        print("[     ]")
+
+    if number == 3:
+        print("[-----]") 
+        print("[0    ]")
+        print("[  0  ]")
+        print("[    0]")
+        print("[     ]")
+
+    if number == 4:
+        print("[-----]") 
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[     ]")
+
+    if number == 5: 
+        print("[-----]") 
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+
+    if number == 6:
+        print("[-----]") 
+        print("[0 0 0]")
+        print("[     ]")
+        print("[0 0 0]")
+        print("[-----]")    
+
+    print(number)
+    a = input("Do you want to continue? yes or no")
+
+
+#prints a random value from the list
+my_list = [1,2,3,4,5,6]
+print('Random number from list:', random.choice('1,2,3,4,5,6,'))
+print('Random number from list:', random.choice('1,2,3,4,5,6,'))
+
+
+
+
+
+1. #breifly describe what your program is about?It supose to repeat it by useing random numbers.
+
+
+2. #What probblem is your program trying to solve?The problem its trying to solve is do you 
+#want to continue or not.
+
+3. #How will your program solve this problem? It would solve this problem by choosing Yes or No to 
+#continue or not.
+
+4. #Are there any apps that similar to your program?yes there is a game that is called bored king
+
+
+5. #Who is this program designed for? Please describe the user who will be
+   #useing the program by describing three(3) things about them and how it relates to them 
+   #using the program.
+#The program is use for the public/everybody is free to use this.The user who would use this is my
+#teacher.Three thing's about them and how it relates using this program is by Mr.Ian having to come
+#to work every day accept monday,he also comes to work 4 days out of the week,and he acyually does those
+#thing's.
---
+++ ZionProject.py
@@ -0,0 +1,81 @@
+#def clothingLogic():
+    #print("great, time to get dressed!")
+user_welcome = input('Welcome Princess Zion, please enter your password!')
+
+if user_welcome == 'arianagrande505':
+    print('Time to choose your outfit!')
+
+if user_welcome == 'dingdog':
+   print('Thats Incorrect Please try again') 
+
+if user_welcome == 'ringring':
+   print('Thats Incorrect, Please Try again')   
+    
+    
+user_mood= input('How are you feeling today!: ')
+
+user_mood == 'good'
+print('thats Wonderful!')
+    
+    #clothingLogic()
+
+#print('Awwe so sorry!  ') 
+
+#user_mood= input('what is your mood: ')
+
+while user_mood == 'sad':
+    print('thats not good')
+    user_mood= input('what is your mood:')
+
+#print('Awwe what happened?') 
+
+#user_mood != 'happy':
+#    print('hi')
+#user_mood = input('what is your mood?')
+
+#print('Thats wonderful  ')
+user_clothes= input('Please choose your clothes')
+
+if user_clothes == 'dress please!':
+    print('thats a wonderful choice!')
+
+if user_clothes == 'shirt please':
+    print('great choice!') 
+
+if user_clothes == 'pants please!':
+    print('nice choice')    
+    
+user_exit= input('would u like to take a picture? please respond with yes or no')
+
+if user_exit == 'yes':
+    print('Ok!')
+
+if user_exit == 'dingdog':
+   print('Thats Incorrect Please try again') 
+
+if user_exit == 'ringring':
+   print('Thats Incorrect, Please Try again')  
+user_snap = input(' 3....2...1..!')
+user_leaves = input('Do you like it?')
+
+if user_leaves == 'Yes, I love it!':
+    print('Ok saving to your memory!')
+
+if user_leaves == 'dingdog':
+   print('Thats Incorrect Please try again') 
+
+if user_leaves == 'ringring':
+   print('Thats Incorrect, Please Try again')  
+
+user_bye = input('Loading..')
+
+if user_bye == 'Thx for taking my picture':
+    print('Your Welcome!, Have a wondeful day with your awesome outfit Princess Zion!!')
+
+if user_bye == 'dingdog':
+   print('Thats Incorrect Please try again') 
+
+if user_bye == 'ringring':
+   print('Thats Incorrect, Please Try again')  
+
+user_cya = input('Bye Bye!') 
